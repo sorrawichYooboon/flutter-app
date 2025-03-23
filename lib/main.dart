356f92a564e50137bc778fefd1e5d9eef2422d94
Sorrawich Yooboon (Ko) <sorrawich.yoob@kkpfg.com>
2025-03-23 17:45:35 +0700

    chore:remove duplicate variable index

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,6 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  int selectedIndex = 0;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
